96ada24f05ae488ce364dc7589c03eef89bed9b5
Zbigniew Matysek <matysek.zbigniew@gmail.com>
2025-09-28 19:21:48 +0200

    give script some love

--- install.sh
+++ install.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
+# shellcheck disable=SC3010
+# shellcheck disable=SC3043
+
 set -eu
+
+DEBUG=0
+if [ "${1-}" = "--debug" ]; then
+    DEBUG=1
+    echo "Running in debug mode"
+fi
+
 
 is_installed() {
     command -v "$1" >/dev/null 2>&1
@@ -25,6 +35,14 @@ install_fonts() {
     rm -rf "$tmp_zip" "$tmp_dir"
 }
 
+run_command() {
+    if [ "$DEBUG" = "1" ]; then
+        "$@"
+    else
+        "$@" >/dev/null 2>&1
+    fi
+}
+
 append_config_line() {
     local config="$1"
     local file="$2"
@@ -44,20 +62,12 @@ ensure_supported_os() {
     fi
 }
 
-print_installation_status() {
-    for s in "mise" "brew" "starship" "ghostty" "aerospace" "docker" "nvim" "code" "node" "pnpm" "python" "uv";
-    do
-        if is_installed $s; then
-            echo "✅ $s is installed!"
-        fi
-    done
-}
+echo "
+   ▘  ▘▗
+██ ▌▛▌▌▜▘  ▛▛▌▀▌▛▘
+██ ▌▌▌▌▐▖▗ ▌▌▌█▌▙▖
 
-print_end_message() {
-    print_installation_status
-    echo ""
-    echo "To enable tools in the current terminal session, run: \"source ~/.zshrc\""
-}
+"
 
 ensure_supported_os
 
@@ -68,56 +78,102 @@ install_fonts \
 echo "Done!"
 
 if ! is_installed mise; then
-    curl https://mise.run | sh
-    append_config_line 'eval "$(mise activate zsh)"' "$HOME/.zshrc"
+    printf "Installing Mise... "
+    run_command curl https://mise.run | sh
+    echo "Done!"
 fi
 
 if ! is_installed brew; then
+    printf "Installing Homebrew... "
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo "Done!"
 fi
 
 if ! is_installed starship; then
-    curl -sS https://starship.rs/install.sh | sh -s -- --yes
-    append_config_line 'eval "$(starship init zsh)"' "$HOME/.zshrc"
+    printf "Installing Starship... "
+    run_command curl -sS https://starship.rs/install.sh | sh -s -- --yes
+    echo "Done!"
 fi
 
 if ! is_installed ghostty; then
-    brew install --cask ghostty
-    append_config_line "keybind = global:ctrl+\`=toggle_quick_terminal" "$HOME/Library/Application Support/com.mitchellh.ghostty/config"
+    printf "Installing Ghostty... "
+    run_command brew install --cask ghostty
+    echo "Done!"
 fi
 
-
 if ! is_installed aerospace; then
-    brew install --cask nikitabobko/tap/aerospace
+    run_command brew install --cask nikitabobko/tap/aerospace
     
     #check if config file exists, if not download default config
     if [[ ! -f "$HOME/.aerospace.toml" ]]; then
-        curl -o "$HOME/.aerospace.toml" "https://initmac.falseinput.com/configs/aerospace.toml"
+        run_command curl -o "$HOME/.aerospace.toml" "https://initmac.falseinput.com/configs/aerospace.toml"
     fi
 fi
 
 if ! is_installed docker; then
-    curl -sS "https://desktop.docker.com/mac/main/arm64/Docker.dmg" -o "/tmp/Docker.dmg"
-    cp -R "/Volumes/Docker/Docker.app" /Applications
-    rm /tmp/Docker.dmg
+    printf "Installing Docker... "
+    run_command curl -sS "https://desktop.docker.com/mac/main/arm64/Docker.dmg" -o "/tmp/Docker.dmg"
+    run_command cp -R "/Volumes/Docker/Docker.app" /Applications
+    run_command rm /tmp/Docker.dmg
+    echo "Done!"
 fi
 
 if ! is_installed nvim; then
-    brew install neovim
-    alled_config_line "alias n='nvim'" "$HOME/.zshrc"
+    printf "Installing Neovim... "
+    run_command brew install neovim
+    echo "Done!"
 fi
 
 if ! is_installed code && ! is_app_installed "Visual Studio Code"; then
-    brew install --cask visual-studio-code
-    append_config_line 'export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"' "$HOME/.zshrc"
+    printf "Installing Visual Studio Code... "
+    run_command brew install --cask visual-studio-code
+    echo "Done!"
 fi
 
+printf "Configuring shell... "
+append_config_line "alias n=nvim" "$HOME/.zshrc"
+append_config_line "export PATH=\"$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin\"" "$HOME/.zshrc"
+append_config_line "keybind = global:ctrl+\`=toggle_quick_terminal" "$HOME/Library/Application Support/com.mitchellh.ghostty/config"
+append_config_line 'eval "$(starship init zsh)"' "$HOME/.zshrc"
+append_config_line 'eval "$(~/.local/bin/mise activate zsh)"' "$HOME/.zshrc"
+echo "Done!"
+
+
 # Javascript
-mise use --global node@lts >/dev/null 2>&1
-mise use --global pnpm >/dev/null 2>&1
+if ! is_installed node; then
+    printf "Installing NodeJS... "
+    run_command mise use --global node@lts
+    echo "Done!"
+fi
+
+if ! is_installed pnpm; then
+    printf "Installing PNPM... "
+    run_command mise use --global pnpm
+    echo "Done!"
+fi
 
 # Python
-mise use --global python@latest >/dev/null 2>&1
-mise use --global uv >/dev/null 2>&1
+if ! is_installed python; then
+    printf "Installing Python... "
+    run_command mise use --global python@latest
+    echo "Done!"
+fi
 
-print_end_message
+if ! is_installed uv; then
+    printf "Installing uv... "
+    run_command mise use --global uv
+    echo "Done!"
+fi
+
+# Bash
+if ! is_installed shellcheck; then
+    printf "Installing ShellCheck... "
+    run_command mise use --global shellcheck
+    echo "Done!"
+fi
+
+# Final message
+echo ""
+echo "All done!"
+echo ""
+echo "To see changes immediately, run: source ~/.zshrc"
